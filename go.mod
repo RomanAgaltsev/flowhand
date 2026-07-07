e4b8ebb37f4dc0e6958e8b9e06577748fc3ad1d4
Roman Agaltsev <roman-agalcev@yandex.ru>
2026-07-07 14:18:40 +0300

    feat(obs): OTel meter provider + runtime metrics + debug handler

--- go.mod
+++ go.mod
@@ -9,15 +9,20 @@ require (
 	github.com/knadh/koanf/providers/file v1.2.1
 	github.com/knadh/koanf/providers/posflag v1.0.1
 	github.com/knadh/koanf/v2 v2.3.5
+	github.com/prometheus/client_golang v1.23.2
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/pflag v1.0.9
 	github.com/stretchr/testify v1.11.1
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.69.0
 	go.opentelemetry.io/otel v1.44.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.44.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.44.0
 	go.opentelemetry.io/otel/sdk v1.44.0
+	go.opentelemetry.io/otel/sdk/metric v1.44.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -31,12 +36,17 @@ require (
 	github.com/knadh/koanf/maps v0.1.2 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.66.1 // indirect
+	github.com/prometheus/procfs v0.16.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.44.0 // indirect
 	go.opentelemetry.io/otel/metric v1.44.0 // indirect
 	go.opentelemetry.io/otel/trace v1.44.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
---
+++ internal/obs/metrics.go
@@ -0,0 +1,80 @@
+package obs
+
+import (
+	"context"
+	"net/http"
+	"net/http/pprof"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
+
+	"github.com/RomanAgaltsev/flowhand/internal/config"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func NewMeterProvider(ctx context.Context, cfg config.Config) (*metric.MeterProvider, error) {
+	meterExporter, err := otlpmetricgrpc.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var deploymentEnvironmentName attribute.KeyValue
+	switch cfg.Env {
+	case "dev":
+		deploymentEnvironmentName = semconv.DeploymentEnvironmentNameDevelopment
+	case "staging":
+		deploymentEnvironmentName = semconv.DeploymentEnvironmentNameStaging
+	case "prod":
+		deploymentEnvironmentName = semconv.DeploymentEnvironmentNameProduction
+	}
+
+	resource, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(cfg.Obs.ServiceName),
+			semconv.ServiceVersion(cfg.Version),
+			deploymentEnvironmentName,
+		),
+	)
+
+	view := metric.NewView(
+		metric.Instrument{
+			Name: "latency",
+		},
+		metric.Stream{
+			Aggregation: metric.AggregationExplicitBucketHistogram{
+				Boundaries: []float64{5, 10, 20, 30, 40, 50, 75, 100, 250, 500, 1000},
+			},
+		},
+	)
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(
+			metric.NewPeriodicReader(meterExporter),
+		),
+		metric.WithResource(resource),
+		metric.WithView(view),
+	)
+
+	err = runtime.Start(runtime.WithMinimumReadMemStatsInterval(5 * time.Second))
+	if err != nil {
+		return nil, err
+	}
+
+	return meterProvider, nil
+}
+
+func DebugHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+
+	return mux
+}
