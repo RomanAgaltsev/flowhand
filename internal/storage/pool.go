62f43832d932deb8fb942d11193afa822b61249e
Roman Agaltsev <roman-agalcev@yandex.ru>
2026-07-15 12:06:45 +0300

    feat(storage): pgx pool + otelpgx + integration test

---
+++ internal/storage/pool.go
@@ -0,0 +1,23 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/exaring/otelpgx"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/RomanAgaltsev/flowhand/internal/config"
+)
+
+func NewPool(ctx context.Context, cfg config.DB) (*pgxpool.Pool, error) {
+	pcfg, err := pgxpool.ParseConfig(cfg.DSN)
+	if err != nil {
+		return nil, fmt.Errorf("parse dsn: %w", err)
+	}
+	pcfg.MaxConns = cfg.MaxConns
+	pcfg.MinConns = cfg.MinConns
+	pcfg.MaxConnLifetime = cfg.MaxConnLifetime
+	pcfg.ConnConfig.Tracer = otelpgx.NewTracer()
+	return pgxpool.NewWithConfig(ctx, pcfg)
+}
